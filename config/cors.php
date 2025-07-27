b239df7087b5e8c9ca03fa565e60e826b7b98ab9
DESKTOP-FLHQLEH\Eduardo Villanueva <eyam809@gmail.com>
2025-07-27 15:50:02 -0600

    testRender13

--- config/cors.php
+++ config/cors.php
@@ -6,18 +6,13 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -28,13 +23,5 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
-    'paths' => ['api/*'],
-
-    'paths' => ['api/*'],
-    'allowed_origins' => ['*'],
-
-
-
 
 ];
